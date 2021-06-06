b50ced74e4a0cabfde7f339e6ad092e091d87512
shruti <shruti2000srivastava@gmail.com>
2021-06-06 22:30:47 +0530

    flutter project created

---
+++ lib/main.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+import 'package:to_do_app/models/constants.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'ToDoey',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'ToDoey'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      color: Colors.yellow,
+      home: SafeArea(
+        child: DefaultTabController(
+          length: 3,
+          child: Scaffold(
+            body: Stack(
+              children: <Widget>[
+                TabBarView(
+                  children: [
+                    Container(
+                      color: Color(0xff212128),
+                    ),
+                    Container(
+                      color: Colors.orange,
+                    ),
+                    Container(
+                      color: Colors.lightGreen,
+                    ),
+                  ],
+                ),
+                Container(
+                  padding: EdgeInsets.only(left: 50.0),
+                  height: 180.0,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(50.0),
+                        bottomRight: Radius.circular(50.0)),
+                    color: Colors.white,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text(
+                        "Intray",
+                        style: kIntrayTextStyle,
+                      ),
+                      Container(),
+                    ],
+                  ),
+                ),
+                Container(
+                  height: 80.0,
+                  width: 80.0,
+                  margin: EdgeInsets.only(
+                      top: 150.0,
+                      left: MediaQuery.of(context).size.width * 0.5 - 40),
+                  child: FloatingActionButton(
+                    onPressed: () {},
+                    child: Icon(
+                      Icons.add,
+                      size: 70.0,
+                    ),
+                    backgroundColor: mustard,
+                  ),
+                ),
+              ],
+            ),
+            appBar: AppBar(
+                elevation: 0.0,
+                bottom: TabBar(
+                  tabs: [
+                    Tab(
+                      icon: Icon(Icons.home),
+                    ),
+                    Tab(
+                      icon: Icon(Icons.rss_feed),
+                    ),
+                    Tab(
+                      icon: Icon(Icons.perm_identity),
+                    ),
+                  ],
+                  labelColor: darkgrey,
+                  unselectedLabelColor: mustard,
+                  indicatorSize: TabBarIndicatorSize.label,
+                  indicatorPadding: EdgeInsets.all(5.0),
+                  indicatorColor: Colors.transparent,
+                ),
+                backgroundColor: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/models/constants.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+Color mustard = Color(0xffffb565);
+Color darkgrey = Color(0xff212128);
+Color iconColor = Color(0xff707070);
+
+var kIntrayTextStyle = TextStyle(
+    fontFamily: 'Avenir',
+    fontSize: 60.0,
+    fontWeight: FontWeight.bold,
+    color: darkgrey);
